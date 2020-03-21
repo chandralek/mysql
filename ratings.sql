54e140f271755edc5227da10b4b5a736ff40e134
KalluruS <>
2020-03-21 21:19:10 +0530

    --- Auto-Git Commit ---

--- ratings.sql
+++ ratings.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE ratings
+CREATE DATABASE IF NOT EXISTS ratings
 DEFAULT CHARACTER SET 'utf8';
 
 USE ratings;
 
-CREATE TABLE ratings (
+CREATE TABLE IF NOT EXISTS ratings (
     sku varchar(80) NOT NULL,
     avg_rating DECIMAL(3, 2) NOT NULL,
     rating_count INT NOT NULL,
